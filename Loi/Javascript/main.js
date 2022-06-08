8f439229a6d958ee79deefff70330c5ecc45b582
Leon Smit <lsmit1994@gmail.com>
2022-06-08 12:21:40 +0200

    commit

---
+++ Loi/Javascript/main.js
@@ -0,0 +1,9 @@
+alert('Welcome to Quiz Ninja!');
+
+const question = "What is Superman's real name?"
+const answer = prompt(question);
+alert(`You answered ${answer}`);
+
+
+
+
---
+++ Loi/Javascript/test.js
@@ -0,0 +1,3 @@
+const vraag = "Typ hier uw tekst";
+const antwoord = prompt(vraag);
+alert(`Uw tekst: ${antwoord} \nDe eerste spatie staat op positie : ` + antwoord.indexOf(" "));
